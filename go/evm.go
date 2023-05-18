fe3d0167ce5a08a2ab15ba90b92e7174214d5d19
MB <michael.dburke56@gmail.com>
2023-05-18 16:17:07 -0500

    Create new bigInt implemented. Must resolve byte to string.

--- go/evm.go
+++ go/evm.go
@@ -6,20 +6,29 @@ import (
 )
 
 func Evm(code []byte) ([]*big.Int, bool) {
+    fmt.Println("Code:", code)
+
 	var stack []*big.Int
 	pc := 0
 
 	for pc < len(code) {
 		op := code[pc]
-		pc++
+        fmt.Println("op:", op)
 
         switch op {
-        case byte(00):
-            fmt.Println("Opcode is 00")
-            stack = append(stack, op)
+        case byte(0):
+            fmt.Println("Opcode is 0", op)
+            
+            opBigInt := new(big.Int)
+            // Must convert byte to string
+            opBigInt.SetString(op, 16)
+
+            stack = append(stack, opBigInt)
         default:
             fmt.Println("Does not reconize opcode")
         }
+	
+        pc++
 	}
 
 	return stack, true
