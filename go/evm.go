478e092d36a0254831690f08cfcbb0fa624cf480
MB <michael.dburke56@gmail.com>
2023-05-19 04:47:23 -0500

    Implement PUSH0.

--- go/evm.go
+++ go/evm.go
@@ -13,11 +13,14 @@ func Evm(code []byte) ([]*big.Int, bool) {
 
 	for pc < len(code) {
 		op := code[pc]
-        fmt.Println("op:", op)
 
         switch op {
         case byte(0):
             break
+        case byte(0x5f):
+            opBigInt := new(big.Int)
+            opBigInt.SetString("0x5f", 16)
+            stack = append(stack, opBigInt)
         default:
             fmt.Println("Does not reconize opcode")
         }
@@ -27,14 +30,3 @@ func Evm(code []byte) ([]*big.Int, bool) {
 
 	return stack, true
 }
-            /*
-            fmt.Println("Opcode is 0", op)
-            
-            opBigInt := new(big.Int)
-            // Must convert byte to string
-            opBigInt.SetString("0x00", 16)
-
-            fmt.Println("opBigInt:", opBigInt)
-
-            stack = append(stack, opBigInt)
-            */
