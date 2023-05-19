ef225f3d8e311e49126b9f4fac23feb45da175f0
MB <michael.dburke56@gmail.com>
2023-05-18 20:31:09 -0500

    Cleaned up approach to fulill first test.

--- go/evm.go
+++ go/evm.go
@@ -17,13 +17,7 @@ func Evm(code []byte) ([]*big.Int, bool) {
 
         switch op {
         case byte(0):
-            fmt.Println("Opcode is 0", op)
-            
-            opBigInt := new(big.Int)
-            // Must convert byte to string
-            opBigInt.SetString(op, 16)
-
-            stack = append(stack, opBigInt)
+            break
         default:
             fmt.Println("Does not reconize opcode")
         }
@@ -33,3 +27,14 @@ func Evm(code []byte) ([]*big.Int, bool) {
 
 	return stack, true
 }
+            /*
+            fmt.Println("Opcode is 0", op)
+            
+            opBigInt := new(big.Int)
+            // Must convert byte to string
+            opBigInt.SetString("0x00", 16)
+
+            fmt.Println("opBigInt:", opBigInt)
+
+            stack = append(stack, opBigInt)
+            */
