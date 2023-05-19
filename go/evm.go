a0da73059b2ff920dc26e3fc201deba619be92e1
MB <michael.dburke56@gmail.com>
2023-05-19 15:03:41 -0500

    Edit to prepend data to stack array(rather than append).

--- go/evm.go
+++ go/evm.go
@@ -26,49 +26,49 @@ func Evm(code []byte) ([]*big.Int, bool) {
             pc++
 
         case byte(0x60)://PUSH1
-            stack = append(stack, parseBigInt(code, pc, 1))
+            stack = append([]*big.Int{parseBigInt(code, pc, 1)}, stack...)
             pc++
 
         case byte(0x61)://PUSH2
-            stack = append(stack, parseBigInt(code, pc, 2))
+            stack = append([]*big.Int{parseBigInt(code, pc, 2)}, stack...)
             pc += 3
 
         case byte(0x63)://PUSH4
-            stack = append(stack, parseBigInt(code, pc, 4))
+            stack = append([]*big.Int{parseBigInt(code, pc, 4)}, stack...)
             pc += 4
 
         case byte(0x64)://PUSH5
-            stack = append(stack, parseBigInt(code, pc, 5))
+            stack = append([]*big.Int{parseBigInt(code, pc, 5)}, stack...)
             pc += 5
 
         case byte(0x65)://PUSH6
-            stack = append(stack, parseBigInt(code, pc, 6))
+            stack = append([]*big.Int{parseBigInt(code, pc, 6)}, stack...)
             pc += 6
 
         case byte(0x66)://PUSH7
-            stack = append(stack, parseBigInt(code, pc, 7))
+            stack = append([]*big.Int{parseBigInt(code, pc, 7)}, stack...)
             pc += 6
 
         case byte(0x67)://PUSH8
-            stack = append(stack, parseBigInt(code, pc, 8))
+            stack = append([]*big.Int{parseBigInt(code, pc, 8)}, stack...)
             pc += 8
 
         case byte(0x68)://PUSH9
-            stack = append(stack, parseBigInt(code, pc, 9))
+            stack = append([]*big.Int{parseBigInt(code, pc, 9)}, stack...)
             pc += 9
 
         case byte(0x69)://PUSH10
-            stack = append(stack, parseBigInt(code, pc, 10))
+            stack = append([]*big.Int{parseBigInt(code, pc, 10)}, stack...)
             pc += 10
 
         case byte(0x6A)://PUSH11
-            stack = append(stack, parseBigInt(code, pc, 11))
+            stack = append([]*big.Int{parseBigInt(code, pc, 11)}, stack...)
             pc += 11
 
 
         case byte(0x7F)://PUSH32
             fmt.Println("hit 7F")
-            stack = append(stack, parseBigInt(code, pc, 32))
+            stack = append([]*big.Int{parseBigInt(code, pc, 32)}, stack...)
             pc += 32
 
         default:
